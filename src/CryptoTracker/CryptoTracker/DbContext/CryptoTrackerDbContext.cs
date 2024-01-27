624ce6ce23b74c7744c3fdf428dadfaf7be05298
t.humer <t.humer@gmail.com>
2024-01-27 17:46:57 +0100

    CryptoTrackerDbContext Fix

--- src/CryptoTracker/CryptoTracker/DbContext/CryptoTrackerDbContext.cs
+++ src/CryptoTracker/CryptoTracker/DbContext/CryptoTrackerDbContext.cs
@@ -5,7 +5,6 @@ namespace CryptoTracker
 {
     public class CryptoTrackerDbContext : DbContext
     {
-        public DbSet<FiatTransaction> FiatTransactions { get; set; }
         public DbSet<CryptoTransaction> CryptoTransactions { get; set; }
         public DbSet<CryptoTrade> CryptoTrades { get; set; }
 
@@ -15,27 +14,8 @@ namespace CryptoTracker
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<FiatTransaction>().HasKey(f => f.Id);
             modelBuilder.Entity<CryptoTransaction>().HasKey(c => c.Id);
             modelBuilder.Entity<CryptoTrade>().HasKey(c => c.Id);
-
-            modelBuilder.Entity<CryptoTransaction>()
-                .Property(c => c.Quantity);
-
-            modelBuilder.Entity<CryptoTransaction>()
-                .Property(c => c.Wallet);
-
-            modelBuilder.Entity<CryptoTransaction>()
-                .Property(c => c.Address);
-
-            modelBuilder.Entity<CryptoTransaction>()
-                .Property(c => c.Fee);
-
-            modelBuilder.Entity<CryptoTrade>()
-                .Property(c => c.Quantity);
-
-            modelBuilder.Entity<CryptoTrade>()
-                .Property(c => c.Price);
 
             base.OnModelCreating(modelBuilder);
         }
