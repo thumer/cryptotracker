b0090f80b92627e1975fd24fa35004166a6a3e21
t.humer <t.humer@gmail.com>
2024-01-28 16:25:04 +0100

    BinanceDepositImporter

--- src/CryptoTracker/CryptoTracker/Import/BinanceDepositImporter.cs
+++ src/CryptoTracker/CryptoTracker/Import/BinanceDepositImporter.cs
@@ -1,5 +1,6 @@
 ﻿using CryptoTracker.Entities;
 using CryptoTracker.Import.Objects;
+using CsvHelper;
 using CsvHelper.Configuration;
 
 namespace CryptoTracker.Import
@@ -17,8 +18,33 @@ namespace CryptoTracker.Import
                         Delimiter = ";",
                     };
 
+        protected override void OnCsvReaderCreated(CsvReader reader)
+        {
+            base.OnCsvReaderCreated(reader);
+            reader.Context.TypeConverterCache.AddConverter<DateTimeOffset>(new UtcDateTimeConverter());
+        }
+
         protected override async Task OnImport(ImportArgs args, IEnumerable<BinanceDeposit> records)
         {
+            foreach (var record in records)
+            {
+                var transaction = new CryptoTransaction
+                {
+                    TransactionType = TransactionType.Receive,
+                    Wallet = args.Wallet,
+                    DateTime = record.Date.LocalDateTime,
+                    Symbol = record.Coin,
+                    Quantity = record.Amount + record.TransactionFee,
+                    Fee = record.TransactionFee,
+                    TransactionId = record.TXID,
+                    Address = record.Address,
+                    Network = record.Network,
+                    Comment = record.Comment
+                };
+                DbContext.Add(transaction);
+            }
+
+            await DbContext.SaveChangesAsync();
         }
     }
 }
