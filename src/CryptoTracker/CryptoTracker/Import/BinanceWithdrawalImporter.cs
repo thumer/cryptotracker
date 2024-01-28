553f1bc46c09dcbc8858e00f75b31c74e42be368
t.humer <t.humer@gmail.com>
2024-01-28 16:28:43 +0100

    BinanceWithdrawalImporter

--- src/CryptoTracker/CryptoTracker/Import/BinanceWithdrawalImporter.cs
+++ src/CryptoTracker/CryptoTracker/Import/BinanceWithdrawalImporter.cs
@@ -1,6 +1,8 @@
 ﻿using CryptoTracker.Client.Pages;
+using CryptoTracker.Entities;
 using CryptoTracker.Import.Objects;
 using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace CryptoTracker.Import
 {
@@ -11,9 +13,39 @@ namespace CryptoTracker.Import
         {
         }
 
+        protected override CsvConfiguration CreateCsvConfiguration()
+                    => new CsvConfiguration(new System.Globalization.CultureInfo("de-AT"))
+                    {
+                        Delimiter = ";",
+                    };
+
+        protected override void OnCsvReaderCreated(CsvReader reader)
+        {
+            base.OnCsvReaderCreated(reader);
+            reader.Context.TypeConverterCache.AddConverter<DateTimeOffset>(new UtcDateTimeConverter());
+        }
+
         protected override async Task OnImport(ImportArgs args, IEnumerable<BinanceWithdrawal> records)
         {
-            throw new NotImplementedException();
+            foreach (var record in records)
+            {
+                var transaction = new CryptoTransaction
+                {
+                    TransactionType = TransactionType.Send,
+                    Wallet = args.Wallet,
+                    DateTime = record.Date.LocalDateTime,
+                    Symbol = record.Coin,
+                    Quantity = record.Amount + record.TransactionFee,
+                    Fee = record.TransactionFee,
+                    TransactionId = record.TXID,
+                    Address = record.Address,
+                    Network = record.Network,
+                    Comment = record.Comment
+                };
+                DbContext.Add(transaction);
+            }
+
+            await DbContext.SaveChangesAsync();
         }
     }
 }
