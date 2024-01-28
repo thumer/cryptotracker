0a8ee3095004a26fb3c17d09e42e63e917d2a7a9
t.humer <t.humer@gmail.com>
2024-01-28 19:38:44 +0100

    MetamaskTransactionImporter

--- src/CryptoTracker/CryptoTracker/Import/MetamaskTransactionImporter.cs
+++ src/CryptoTracker/CryptoTracker/Import/MetamaskTransactionImporter.cs
@@ -1,19 +1,49 @@
 ﻿using CryptoTracker.Client.Pages;
+using CryptoTracker.Entities;
 using CryptoTracker.Import.Objects;
 using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace CryptoTracker.Import
 {
     public class MetamaskTransactionImporter : ImporterBase<MetamaskTransaction>
     {
-        public MetamaskTransactionImporter(CryptoTrackerDbContext dbContext) 
+        public MetamaskTransactionImporter(CryptoTrackerDbContext dbContext)
             : base(dbContext)
         {
         }
 
+        protected override CsvConfiguration CreateCsvConfiguration()
+            => new CsvConfiguration(new System.Globalization.CultureInfo("de-AT"))
+            {
+                Delimiter = ";",
+            };
+
+        protected override void OnCsvReaderCreated(CsvReader reader)
+        {
+            base.OnCsvReaderCreated(reader);
+            reader.Context.TypeConverterCache.AddConverter<DateTimeOffset>(new UtcDateTimeConverter());
+        }
+
         protected override async Task OnImport(ImportArgs args, IEnumerable<MetamaskTransaction> records)
         {
-            throw new NotImplementedException();
+            foreach (var record in records)
+            {
+                var transaction = new CryptoTransaction
+                {
+                    TransactionType = record.Typ == "Eingang" ? TransactionType.Receive : TransactionType.Send,
+                    Wallet = args.Wallet,
+                    DateTime = record.Datum.LocalDateTime,
+                    Symbol = record.Coin,
+                    Quantity = record.Amount + record.TransactionFee,
+                    Fee = record.TransactionFee,
+                    Network = record.Network,
+                    Comment = record.Kommentar
+                };
+                DbContext.Add(transaction);
+            }
+
+            await DbContext.SaveChangesAsync();
         }
     }
 }
